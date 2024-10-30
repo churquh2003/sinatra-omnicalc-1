dc266ca37b3ade94fc6c41c09acca0a1584bed61
churquh2003 <curquhart@uchicago.edu>
2024-10-30 05:52:27 +0000

    new commit 3

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ end
 get("/goodbye") do
   erb(:bye)
 end 
+
+get("/square/new") do
+  erb(:new_square_calc)
+
+end
+
+get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
